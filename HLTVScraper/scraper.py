3e6a7a29c274a1e6ed9e6b2e2d35347cb54742ca
dkulinov <53202097+dkulinov@users.noreply.github.com>
2021-11-30 22:28:04 -0600

    partial

--- HLTVScraper/scraper.py
+++ HLTVScraper/scraper.py
@@ -21,20 +21,28 @@ def getUpcomingMatches(predefinedFilter: MatchType = MatchType.TopTier) -> [Matc
     html = res.read()
 
     theSoup = soup(html, "html.parser")
+    body = theSoup.body
+    mainContent = body.find(class_='bgPadding')
+    print(mainContent)
     team1s = theSoup.find_all(class_='team1')
     team2s = theSoup.find_all(class_='team2')
     numMatches = len(team1s)
-    for i in range(numMatches):
-        print("\nMatch: " + team1s[i].get_text().strip() + " vs " + team2s[i].get_text().strip())
+    # for i in range(numMatches):
+    #     print("\nMatch: " + team1s[i].get_text().strip() + " vs " + team2s[i].get_text().strip())
 
+    # for child in theSoup.contents[2].contents:
+    #     if(child.contents.contains())
+    #     print("CHILD: ", child)
     return [Match()]
 
 
 def getUpcomingMatchesByTeam(team: str) -> [Match]:
     pass
 
+def getUpcomingMatchesByDay(day: str) -> [Match]:
+    pass
 
-#
+# by day and by team
 # def getPastMatches(predefinedFilter: MatchType, team: str= "None") -> [Match]:
 #     return [Match()]
 #

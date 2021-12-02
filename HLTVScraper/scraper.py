3faa9fd8ee37c0cc0bf72772f32f9842c377c8b5
dkulinov <53202097+dkulinov@users.noreply.github.com>
2021-12-01 23:19:48 -0600

    got the section with upcoming matches

--- HLTVScraper/scraper.py
+++ HLTVScraper/scraper.py
@@ -17,18 +17,20 @@ def getUpcomingMatches(predefinedFilter: MatchType = MatchType.TopTier) -> [Matc
     url = baseUrl + "matches"
     url += "?predefinedFilter=" if predefinedFilter != MatchType.Default else ""
     url += predefinedFilter.value
-    print(url)
     req = Request(url, headers=headers)
     res = urlopen(req)
     html = res.read()
 
     theSoup = soup(html, "html.parser")
-    body = theSoup.body
-    mainContent = body.find(class_='bgPadding')
-    print(mainContent)
-    team1s = theSoup.find_all(class_='team1')
-    team2s = theSoup.find_all(class_='team2')
-    numMatches = len(team1s)
+    matchesByDay = theSoup.find(class_='upcomingMatchesContainer')
+    sectionClassname = matchesByDay.div.get('data-zonedgrouping-group-classes')
+    dateClassname = matchesByDay.div.get('data-zonedgrouping-headline-classes')
+    print(sectionClassname)
+    print(dateClassname)
+    # print(matchesByDay)
+    # team1s = theSoup.find_all(class_='team1')
+    # team2s = theSoup.find_all(class_='team2')
+    # numMatches = len(team1s)
     # for i in range(numMatches):
     #     print("\nMatch: " + team1s[i].get_text().strip() + " vs " + team2s[i].get_text().strip())
 
